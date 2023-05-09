c194fbbbe0d363f400a39556d032b273d11ff0b1
huzitaiwo <alhassansahih@gmail.com>
2023-05-09 23:00:11 +0100

    on auth state change

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -10,7 +10,7 @@ export const authReducer = (state, action) => {
     case "LOGOUT":
       return { ...state, user: null };
     case "AUTH_IS_READY":
-      return { ...state, user: action.payload, authIsready: true };
+      return { ...state, user: action.payload, authIsReady: true };
     default:
       return state;
   }
@@ -19,14 +19,14 @@ export const authReducer = (state, action) => {
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
-    authIsready: false,
+    authIsReady: false,
   });
 
   useEffect(() => {
     const unsub = projectAuth.onAuthStateChanged((user) => {
       dispatch({ type: "AUTH_IS_READY", payload: user });
+      unsub();
     });
-    unsub();
   }, []);
 
   console.log("Authcontext state:", state);
