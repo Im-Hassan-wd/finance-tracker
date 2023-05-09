cc7174e813805cf6a47b1a9ce557bf776be9bae8
huzitaiwo <alhassansahih@gmail.com>
2023-05-09 23:47:01 +0100

    transaction component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,16 @@
 //  styles
 import styles from "./Home.module.css";
 
+//components
+import TransationForm from "./TransationForm";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransationForm />
+      </div>
+    </div>
+  );
 }
