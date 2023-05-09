49f3ae3d2b3c24f2b2a05e9c00425d57936ba47c
huzitaiwo <alhassansahih@gmail.com>
2023-05-10 00:31:29 +0100

    adding doc to data base

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,3 +1,4 @@
+import { useAuthContext } from "../../hooks/useAuthContext";
 //  styles
 import styles from "./Home.module.css";
 
@@ -5,11 +6,13 @@ import styles from "./Home.module.css";
 import TransationForm from "./TransationForm";
 
 export default function Home() {
+  const { user } = useAuthContext();
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>transaction list</div>
       <div className={styles.sidebar}>
-        <TransationForm />
+        <TransationForm uid={user.uid} />
       </div>
     </div>
   );
--- src/pages/home/TransationForm.js
+++ src/pages/home/TransationForm.js
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useFirestore } from "../../hooks/useFirestore";
 
-export default function TransationForm() {
+export default function TransationForm({ uid }) {
   const [name, setName] = useState("");
   const [amount, setAmount] = useState("");
+  const { addDocument, response } = useFirestore("transactions");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log({ name, amount });
+    addDocument({ uid, name, amount });
   };
+
+  useEffect(() => {
+    if (response.success) {
+      setName("");
+      setAmount("");
+    }
+  }, [response.success]);
 
   return (
     <>
