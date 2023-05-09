14b2cf90eeb362023e088294f8aa00ab4c261c71
huzitaiwo <alhassansahih@gmail.com>
2023-05-09 17:46:56 +0100

    login and signup page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import './App.css'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// pages and compnents
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
 
-export default App
+export default App;
